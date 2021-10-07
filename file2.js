da837384afd630cd3f4ec89bd6ae0cd3d794268d
Riadh Demo GIT <riadh.gharbi@yaiglobal.com>
2021-10-07 17:46:06 +0100

    updated repo for better use

---
+++ file2.js
@@ -0,0 +1,11 @@
+console.log(object);
+
+return new Promise((resolve, reject) => {
+    
+});
+
+console.log(object);
+array.forEach(currentItem => {
+    
+});
+
---
+++ file5.py
@@ -0,0 +1,7 @@
+s = "string in python"
+print(s)
+if s:
+    print(55)
+else:
+    print('er')
+
